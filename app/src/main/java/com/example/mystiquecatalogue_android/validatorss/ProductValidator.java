71b8622348185324cec618dabdfb1da677fe7e90
Rayna Pavlova <pavlovarayna@abv.bg>
2018-09-05 16:21:38 +0300

    corrected validation

--- app/src/main/java/com/example/mystiquecatalogue_android/validatorss/ProductValidator.java
+++ app/src/main/java/com/example/mystiquecatalogue_android/validatorss/ProductValidator.java
@@ -9,13 +9,46 @@ public class ProductValidator implements Validator<Product> {
     @Override
     public boolean isValid(Product object) {
         return object != null &&
-                isNameValid(object);
+                isNameValid(object) &&
+                isCategoryValid(object) &&
+                isTypeValid(object) &&
+                isUnitsValid(object) &&
+                isIdValid(object) &&
+                isSizeValid(object) &&
+                isNumberValid(object);
 
     }
-
 
     private boolean isNameValid(Product object) {
         return object.getName().length() >= Constants.PRODUCT_NAME_MIN_LENGHT &&
                 object.getName().length() <= Constants.PRODUCT_NAME_MAX_LENGHT;
     }
+
+    private boolean isCategoryValid(Product object) {
+        return object.getCategory().length() >= Constants.PRODUCT_CATEGORY_MIN_LENGHT &&
+                object.getCategory().length() <= Constants.PRODUCT_CATEGORY_MAX_LENGHT;
+    }
+
+    private boolean isTypeValid(Product object) {
+        return object.getType().length() >= Constants.PRODUCT_TYPE_MIN_LENGHT &&
+                object.getType().length() <= Constants.PRODUCT_TYPE_MAX_LENGHT;
+    }
+
+    private boolean isUnitsValid(Product object) {
+        return object.getUnits().length() >= Constants.PRODUCT_UNITS_MIN_LENGHT &&
+                object.getUnits().length() <= Constants.PRODUCT_UNITS_MAX_LENGHT;
+    }
+
+    private boolean isIdValid(Product object) {
+        return object.getId() > 0;
+    }
+
+    private boolean isSizeValid(Product object) {
+        return object.getSize() > 0;
+    }
+
+    private boolean isNumberValid(Product object) {
+        return object.getNumber() > 0;
+    }
+
 }
